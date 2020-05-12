8b260ab10c333ccb6acc14570d145db1be409da9
Brian Espina <espinabrian@gmail.com>
2020-05-12 19:40:41 +0800

    deleted unused files

--- helperFunctions.js
+++
@@ -1,7 +0,0 @@
-export default function composeInjectToElement(obj, element){
-    const newElement = document.createElement('div')
-    newElement.classList.add(obj.className)
-    const output = obj.label + obj.data
-    newElement.textContent = output
-    element.appendChild(newElement)
-}
