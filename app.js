94c29fdd39fb8f4c77648458d34b6f9892cf28bc
Brian Espina <espinabrian@gmail.com>
2020-05-12 19:36:53 +0800

    refactoring code

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
-import globalStatus from './globalStatus.js'
+
 
 const API_URL = 'https://api.covid19api.com/summary'
 const globalComponent = document.getElementById('root')
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchAPI()
+})
 
 async function fetchAPI(){
     try{
         const call = await fetch(API_URL)
         const data = await call.json()
-
-        globalStatus(data, globalComponent)
+        globalStatusComponent(data)
         document.getElementById('loader').classList.add('hide')
-
     }catch(err){console.log(err)}
 }
 
-fetchAPI()
 
+function globalStatusComponent(obj){
+    const globalData = obj.Global
+    Object.entries(globalData).forEach(([key, value]) => {
+        composeInjectToElement(key, value, globalComponent)
+    })
+}
+
+function composeInjectToElement(key, value, element){
+    const newElement = document.createElement('div')
+    const label = key.replace(/([a-z])([A-Z])/g, '$1 $2')
+    newElement.classList.add(key)
+    const output = `${label}: ${value}`
+    newElement.textContent = output
+    element.appendChild(newElement)
+}
